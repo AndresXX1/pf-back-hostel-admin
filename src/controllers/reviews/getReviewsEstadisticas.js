ada6d61cd30f0437c4db0bd349af604e9ffdcca2
AndresveraXX <andres_9_09@hotmail.com>
2024-02-18 06:21:15 -0300

    prueba reviews estadisticas

--- src/controllers/reviews/getReviewsEstadisticas.js
+++ src/controllers/reviews/getReviewsEstadisticas.js
@@ -1,8 +1,12 @@
+const moment = require('moment-timezone');
 const { sequelize, Review } = require("../../db");
 const { Op } = require("sequelize");
 
 const getReviewStatistics = async (req, res) => {
     try {
+        // Obtener la fecha actual en la zona horaria de Buenos Aires
+        const currentDate = moment().tz('America/Argentina/Buenos_Aires').format('YYYY-MM-DD');
+
         const reviewStats = await sequelize.query(
             `SELECT 
                 TO_CHAR(DATE_TRUNC('hour', "createdAt" AT TIME ZONE 'UTC' AT TIME ZONE 'ART'), 'HH24:00') AS "hour",
@@ -10,12 +14,15 @@ const getReviewStatistics = async (req, res) => {
             FROM 
                 "Review"
             WHERE 
-                "createdAt" >= CURRENT_DATE - INTERVAL '1 day'
+                "createdAt" >= :currentDate::date - INTERVAL '1 day'
             GROUP BY 
                 TO_CHAR(DATE_TRUNC('hour', "createdAt" AT TIME ZONE 'UTC' AT TIME ZONE 'ART'), 'HH24:00')
             ORDER BY 
                 "hour" ASC;`,
-            { type: sequelize.QueryTypes.SELECT }
+            { 
+                replacements: { currentDate },
+                type: sequelize.QueryTypes.SELECT 
+            }
         );
 
         res.status(200).json(reviewStats);
