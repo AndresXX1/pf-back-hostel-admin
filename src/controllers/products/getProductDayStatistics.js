89a7e973e989072acef98c8a7a1e85a8e7832a21
AndresveraXX <andres_9_09@hotmail.com>
2024-02-21 16:21:09 -0300

    fix estadisticas productos

--- src/controllers/products/getProductDayStatistics.js
+++ src/controllers/products/getProductDayStatistics.js
@@ -28,37 +28,48 @@ const getProductStatistics = async (req, res) => {
         if (productStats.length > 0) {
             res.status(200).json(productStats);
         } else {
-            // Si no hay estadísticas para el día actual, buscar las últimas 3 agrupaciones del día anterior
-            const previousDate = new Date(currentDate);
-            previousDate.setDate(previousDate.getDate() - 1); // Obtener la fecha del día anterior
-            const previousDateString = previousDate.toISOString().split('T')[0]; // Obtener la fecha del día anterior en formato YYYY-MM-DD
-
-            // Consulta SQL para obtener las últimas 3 agrupaciones del día anterior
-            const productStatsPrevious = await sequelize.query(
-                `SELECT 
-                    TO_CHAR("createdAt" AT TIME ZONE 'America/Argentina/Cordoba', 'YYYY-MM-DD') AS "date",
-                    TO_CHAR("createdAt" AT TIME ZONE 'America/Argentina/Cordoba', 'HH24:00') AS "hour",
-                    COUNT(*) AS "productCount"
+            // Obtener la fecha del último día que tenga agrupaciones por hora
+            const lastDateWithStats = await sequelize.query(
+                `SELECT DISTINCT
+                    TO_CHAR("createdAt" AT TIME ZONE 'America/Argentina/Cordoba', 'YYYY-MM-DD') AS "date"
                 FROM 
                     "Product"
                 WHERE
-                    TO_CHAR("createdAt" AT TIME ZONE 'America/Argentina/Cordoba', 'YYYY-MM-DD') = '${previousDateString}'
-                GROUP BY 
-                    TO_CHAR("createdAt" AT TIME ZONE 'America/Argentina/Cordoba', 'YYYY-MM-DD'),
-                    TO_CHAR("createdAt" AT TIME ZONE 'America/Argentina/Cordoba', 'HH24:00')
-                ORDER BY 
-                    "date" ASC,
-                    "hour" ASC
-                LIMIT 3;`, // Limitar a las últimas 3 agrupaciones del día anterior
+                    TO_CHAR("createdAt" AT TIME ZONE 'America/Argentina/Cordoba', 'YYYY-MM-DD') < '${currentDateString}'
+                ORDER BY
+                    "date" DESC
+                LIMIT 1;`,
                 { type: sequelize.QueryTypes.SELECT }
             );
 
-            // Verificar si se obtuvieron estadísticas para el día anterior
-            if (productStatsPrevious.length > 0) {
-                res.status(200).json(productStatsPrevious);
-            } else {
-                // Si no hay estadísticas para el día anterior, devolver un mensaje indicando que no se encontraron estadísticas
+            if (lastDateWithStats.length === 0) {
+                // Si no se encontraron fechas anteriores con estadísticas, devolver un mensaje indicando que no se encontraron estadísticas
                 res.status(404).json({ message: "No se encontraron estadísticas de productos." });
+            } else {
+                const lastDateString = lastDateWithStats[0].date;
+
+                // Consulta SQL para obtener las últimas tres agrupaciones del último día que tenga agrupaciones por hora
+                const productStatsPrevious = await sequelize.query(
+                    `SELECT 
+                        TO_CHAR("createdAt" AT TIME ZONE 'America/Argentina/Cordoba', 'YYYY-MM-DD') AS "date",
+                        TO_CHAR("createdAt" AT TIME ZONE 'America/Argentina/Cordoba', 'HH24:00') AS "hour",
+                        COUNT(*) AS "productCount"
+                    FROM 
+                        "Product"
+                    WHERE
+                        TO_CHAR("createdAt" AT TIME ZONE 'America/Argentina/Cordoba', 'YYYY-MM-DD') = '${lastDateString}'
+                    GROUP BY 
+                        TO_CHAR("createdAt" AT TIME ZONE 'America/Argentina/Cordoba', 'YYYY-MM-DD'),
+                        TO_CHAR("createdAt" AT TIME ZONE 'America/Argentina/Cordoba', 'HH24:00')
+                    ORDER BY 
+                        "date" ASC,
+                        "hour" ASC
+                    LIMIT 3;`, // Limitar a las últimas 3 agrupaciones del último día con estadísticas
+                    { type: sequelize.QueryTypes.SELECT }
+                );
+
+                // Devolver las estadísticas obtenidas
+                res.status(200).json(productStatsPrevious);
             }
         }
     } catch (error) {
