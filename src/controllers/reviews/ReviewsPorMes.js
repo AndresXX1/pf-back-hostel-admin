5e18ae6c2c3ed930d13c371406bcf61555dd589f
AndresveraXX <andres_9_09@hotmail.com>
2024-02-29 11:07:49 -0300

    ddddd

--- src/controllers/reviews/ReviewsPorMes.js
+++ src/controllers/reviews/ReviewsPorMes.js
@@ -8,7 +8,14 @@ const getReviewStatisticsMes = async (req, res) => {
         const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
 
         // Consulta para obtener las estadísticas de reviews para el mes actual
-        const monthStats = await getReviewStatisticsForMonth(firstDayOfMonth, lastDayOfMonth);
+        let monthStats = await getReviewStatisticsForMonth(firstDayOfMonth, lastDayOfMonth);
+
+        // Si no hay estadísticas disponibles para el mes actual, buscar las del mes anterior
+        if (monthStats.length === 0) {
+            const firstDayOfPreviousMonth = new Date(today.getFullYear(), today.getMonth() - 1, 1);
+            const lastDayOfPreviousMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+            monthStats = await getReviewStatisticsForMonth(firstDayOfPreviousMonth, lastDayOfPreviousMonth);
+        }
 
         // Si hay estadísticas disponibles, agruparlas por semana
         if (monthStats.length > 0) {
@@ -17,8 +24,8 @@ const getReviewStatisticsMes = async (req, res) => {
             return res.status(200).json(response);
         }
 
-        // Si no se encuentran estadísticas para el mes actual, devolver un mensaje indicando que no se encontraron estadísticas
-        return res.status(404).json({ message: "No se encontraron estadísticas de reviews para este mes." });
+        // Si no se encuentran estadísticas para el mes actual ni para el mes anterior, devolver un mensaje indicando que no se encontraron estadísticas
+        return res.status(404).json({ message: "No se encontraron estadísticas de reviews para este mes ni para el mes anterior." });
 
     } catch (error) {
         // Capturar cualquier error y enviar una respuesta de error interno del servidor
