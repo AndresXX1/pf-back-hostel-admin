d5813153f6fa473fa9a6431b212f87163f464a69
AndresveraXX <andres_9_09@hotmail.com>
2024-02-29 06:35:32 -0300

    get all reservas

--- src/controllers/recerba/getAllReservas.js
+++ src/controllers/recerba/getAllReservas.js
@@ -1,9 +1,19 @@
 const { Reservas } = require("../../db");
 
 const allReservasHandler = async (req, res) => {
-  try {
+ try {
     const response = await Reservas.findAll({
-      attributes: ['id', 'productId', 'userId', 'startDate', 'endDate', 'totalRooms', 'totalGuests', 'createdAt']
+      include: [
+        {
+          model: User, // Asegúrate de importar el modelo User
+          attributes: ['name'], // Ajusta los atributos según sea necesario
+        },
+        {
+          model: Product, // Asegúrate de importar el modelo Product
+          attributes: ['name'], // Ajusta los atributos según sea necesario
+        },
+      ],
+      attributes: ['id', 'productId', 'userId', 'startDate', 'endDate', 'totalRooms', 'totalGuests', 'createdAt', 'totalAmount']
     });
 
     const mappedReservas = response.map((reserva) => ({
@@ -14,16 +24,19 @@ const allReservasHandler = async (req, res) => {
       endDate: reserva.endDate,
       totalRooms: reserva.totalRooms,
       totalGuests: reserva.totalGuests,
-      createdAt: reserva.createdAt
+      createdAt: reserva.createdAt,
+      totalAmount: reserva.totalAmount,
+      userName: reserva.User.name, // Accede al nombre del usuario a través de la asociación
+      productName: reserva.Product.name, // Accede al nombre del producto a través de la asociación
     }));
 
     return res.status(200).json(mappedReservas);
-  } catch (error) {
+ } catch (error) {
     console.error("Error fetching reservas:", error);
     return res.status(500).json({ message: "Error interno del servidor" });
-  }
+ }
 };
 
 module.exports = {
-  allReservasHandler,
+ allReservasHandler,
 };
